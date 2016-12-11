9489542f029f760eb184099ccbe6c5ad3de73a6d
Ruben Bartelink <ruben@bartelink.com>
2016-12-11 01:00:11 +0000

    Grammar fixes

--- FsUno.Journey/Evolve vs Apply.fsx
+++ FsUno.Journey/Evolve vs Apply.fsx
@@ -3,16 +3,14 @@
 
 *2016-08-25*
 
-I definitly changed the name of the `apply` function to `evolve`.
+I definitely changed the name of the `apply` function to `evolve`.
 
-The name `apply` carries to much mutability semantic, and is to much associated with replay.
+The name `apply` implies too much mutability semantic, and is too much associated with replay.
 
 I also renamed `replay` to `fold` for the same reason.
 
-People tend to get scared of Event Sourcing because of this replay thing... I often hear
+People tend to get scared of Event Sourcing because of this replay thing... I often hear people asking _Will side effects happen again ?_
 
-    Will side effects happen again ?
-
-Using `fold` let people see it as side effects free. It's just a simple computation.
+Using the name `fold` implies it's a simple side-effect-free computation.
 
 *)
